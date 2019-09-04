7aba6b22f7521c13c622d7dd2b4903d38992ed3e
JackY <jack.yujia.yuan@gmail.com>
2019-09-04 11:44:43 +1000

    Refactor reducers for leaner switch statement.

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,20 +5,23 @@ const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  //One way to remove an item in array.
+  // const id = action.id;
+  // const newArray = [...state.results];
+  // newArray.splice(id, 1);
+
+  //Here is another way:
+  const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+  return updateObject(state, {results: updatedArray})
+}
+
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       return updateObject(state, {results : state.results.concat({id: new Date, value: action.result})});
     case actionTypes.DELETE_RESULT:
-    {
-      //One way to remove an item in array.
-      // const id = action.id;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1);
-
-      //Here is another way:
-      const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-      return updateObject(state, {results: updatedArray})};
+      return deleteResult(state, action);
     default:
       return state;
   }
